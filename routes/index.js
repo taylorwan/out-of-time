82ae53b063433cfed643b8ffc48d47567b832147
taylor <taylor@splashthat.com>
2016-11-13 16:50:59 -0500

    added validation and preferences

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,10 @@ router.get('/signup', function(req, res) {
   res.render('signup', { title: 'Sign Up' });
 });
 
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Log In' });
+});
+
 router.get('/about', function(req, res) {
   res.render('about', { title: 'About' });
 });
